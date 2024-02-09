303856a3f33949773adfb24812e2525e28d66312
Jeremy Scatigna <scatignaj@me.com>
2024-02-09 14:33:09 +0100

    feat(setup): fix schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_02_06_031739) do
+ActiveRecord::Schema[7.2].define(version: 2024_02_03_050018) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
