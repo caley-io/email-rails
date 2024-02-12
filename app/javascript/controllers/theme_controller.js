3a581ec90233ff8eef2bc6a5de05387cc74126a5
Austin Wasson <austinpwasson@gmail.com>
2024-02-12 16:32:03 -0700

    sun and moon toggle

--- app/javascript/controllers/theme_controller.js
+++ app/javascript/controllers/theme_controller.js
@@ -2,11 +2,12 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="theme"
 export default class extends Controller {
-  static targets = [ "body"]
+  static targets = [ "body", "moon", "sun"]
 
   connect() {
     this.currentTheme = localStorage.getItem('theme');
     this.setTheme()
+    this.setToggle()
   }
 
   saveTheme(theme) {
@@ -21,13 +22,25 @@ export default class extends Controller {
     }
   }
 
+  setToggle() {
+    if (this.currentTheme === 'dark') {
+      this.sunTarget.classList.add('hidden')
+    } else {
+      this.moonTarget.classList.add('hidden')
+    }
+  }
+
   toggle() {
     let theme = localStorage.getItem('theme');
     if (theme === 'dark') {
       this.bodyTarget.classList.remove('dark')
+      this.sunTarget.classList.remove('hidden')
+      this.moonTarget.classList.add('hidden')
       this.saveTheme('light');
     } else {
       this.bodyTarget.classList.add('dark')
+      this.sunTarget.classList.add('hidden')
+      this.moonTarget.classList.remove('hidden')
       this.saveTheme('dark')
     }
   }
