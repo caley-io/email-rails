5cf038e7a4859e321c606620c3c55bb3e7380e43
Austin Wasson <austinpwasson@gmail.com>
2024-02-23 12:01:24 -0700

    auth adjustments

--- app/controllers/concerns/authentication.rb
+++ app/controllers/concerns/authentication.rb
@@ -2,31 +2,32 @@ module Authentication
   extend ActiveSupport::Concern
 
   included do
-    helper_method :user_signed_in?
+    helper_method :user_signed_in?, :current_user
+  end
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
   end
 
   private
 
   def authenticate_user!
-    if user = User.find_by(id: session[:user_id])
-      Current.user = user
-    else
-      redirect_to new_sessions_url
-    end
+    # TODO: Fix
+    redirect_to new_sessions_url unless user_signed_in?
   end
 
   def user_signed_in?
-    Current.user.present?
+    current_user
   end
 
   def login(user)
-    Current.user = user
     reset_session
     session[:user_id] = user.id
   end
 
   def logout
-    Current.user = nil
+    session[:user_id] = nil
+    @current_user = nil
     reset_session
   end
 end
