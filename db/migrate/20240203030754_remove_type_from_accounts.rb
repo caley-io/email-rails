ed276a9b4373d7ca7b28c8f59b0aa4eb6b6c5718
Austin Wasson <austinpwasson@gmail.com>
2024-02-23 10:23:36 -0700

    remove migrations and reset schema

--- db/migrate/20240203030754_remove_type_from_accounts.rb
+++
@@ -1,5 +0,0 @@
-class RemoveTypeFromAccounts < ActiveRecord::Migration[7.2]
-  def change
-    remove_column :accounts, :type
-  end
-end
--- db/migrate/20240212120134_add_imap_fields_to_accounts.rb
+++
@@ -1,8 +0,0 @@
-class AddImapFieldsToAccounts < ActiveRecord::Migration[7.2]
-  def change
-    add_column :accounts, :imap_server, :string
-    add_column :accounts, :imap_port, :integer
-    add_column :accounts, :imap_ssl, :boolean
-    add_column :accounts, :password, :string
-  end
-end
