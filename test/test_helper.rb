7a750624244262f6dbb9354629debc0b0ee0e39f
Nazeef Ahmad Meer <nazeefahmadmeer@hotmail.com>
2024-03-05 19:24:02 +0000

    Scaffold basic minitest controller/request tests for the emails_controller
    Why?
    - Add basic tests which we can grow as we add more functionality
    
    How?
    - Add basic tests for checking success responses for emails controller
    - Remove unused and incomplete fixtures

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,15 +4,12 @@ require "rails/test_help"
 
 module ActiveSupport
   class TestCase
+    include Devise::Test::IntegrationHelpers
+
     # Run tests in parallel with specified workers
     parallelize(workers: :number_of_processors)
 
     # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
     fixtures :all
-
-    # Add more helper methods to be used by all tests here...
-    def sign_in(user)
-      post sessions_path, params: {email: user.email, password: "password"}
-    end
   end
 end
