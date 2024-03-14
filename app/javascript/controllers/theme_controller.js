1eb68588aa1cc60968a2e135489839c46847b2d3
Austin Wasson <austinpwasson@gmail.com>
2024-03-13 20:31:09 -0600

    update styles and theme controller

--- app/javascript/controllers/theme_controller.js
+++ app/javascript/controllers/theme_controller.js
@@ -2,7 +2,7 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="theme"
 export default class extends Controller {
-  static targets = [ "body", "moon", "sun"]
+  static targets = ["body"]
 
   connect() {
     this.currentTheme = localStorage.getItem('theme');
@@ -14,7 +14,7 @@ export default class extends Controller {
   }
 
   setAppTheme() {
-    if (this.currentTheme === 'dark' || this.currentTheme === null) {
+    if (this.currentTheme === 'dark') {
       this.setDarkTheme()
     } else {
       this.setLightTheme()
@@ -24,21 +24,12 @@ export default class extends Controller {
   // TODO: Fix this ugly mess
   setLightTheme() {
     this.bodyTarget.classList.remove('dark')
-    this.sunTarget.classList.remove('border-transparent')
-    this.sunTarget.classList.add('border-emerald-300')
-    this.moonTarget.classList.add('border-transparent')
-    this.moonTarget.classList.remove('border-emerald-300')
     this.saveTheme('light');
   }
 
   // TODO: Fix this ugly mess
   setDarkTheme() {
     this.bodyTarget.classList.add('dark')
-    this.moonTarget.classList.remove('border-transparent')
-    this.moonTarget.classList.add('border-emerald-300')
-    this.sunTarget.classList.add('border-transparent')
-    this.sunTarget.classList.remove('border-emerald-300')
     this.saveTheme('dark');
   }
-
 }
